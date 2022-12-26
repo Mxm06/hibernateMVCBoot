1a83955ecd1c2a2625e5f108b02bbdf9e26afbb9
Mxm06 <stingermax06@mail.ru>
2022-12-26 19:45:35 +0500

    Task completed

--- src/main/java/springboot/hibernatemvcboot/web/dao/UserDaoImpl.java
+++ src/main/java/springboot/hibernatemvcboot/web/dao/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package springboot.hibernatemvcboot.web.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import springboot.hibernatemvcboot.web.model.User;
@@ -9,15 +10,10 @@ import springboot.hibernatemvcboot.web.model.User;
 import java.util.List;
 @Repository
 public class UserDaoImpl implements UserDao{
-    final private String FROM_USER = "From User";
-    private EntityManager entityManager;
-    // Я честно не знаю, почему не видит бин энтити менеджера, учитывая тот факт, что его создаёт автоматически, гугл ничего не ответил
-    // А программа работает нормально, просто подавить это подчёркивание или я что-то не понимаю?
-    @Autowired
-    public UserDaoImpl(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
 
+    final private String FROM_USER = "From User";
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public void save(User user) {
         entityManager.merge(user);
